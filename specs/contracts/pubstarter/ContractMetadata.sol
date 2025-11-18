bd3fa25a571aa1ab321538f52657dba7f45bfe32
Adam Spitz <adam@acspitz.xyz>
2025-11-18 15:19:51 -0500

    Moved the pubstarter contracts from the specs section into the hardhat project.
    (The hardhat project itself isn't really much more than a thin wrapper around the contracts themselves. Might as well keep them around, so I can run them and test them and stuff.)

--- specs/contracts/pubstarter/ContractMetadata.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0 <0.9.0;
-
-// Useful for debugging. Remove when deploying to a live network.
-// import "hardhat/console.sol";
-
-contract ContractMetadata {
-    event ContractMetadataUpdated(string metadata);
-}
