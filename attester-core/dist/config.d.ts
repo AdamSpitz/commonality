88dd500ba13aab4430ebab0061d32a1ce42a7c2f
Adam Spitz <adam@acspitz.xyz>
2026-04-24 09:05:54 -0400

    Removed dist stuff that shouldn't have been in there.

--- attester-core/dist/config.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function requireEnv(name: string, value?: string | undefined): string;
-export declare function readStringEnv(name: string, fallback: string): string;
-export declare function readNumberEnv(name: string, fallback: number): number;
-//# sourceMappingURL=config.d.ts.map
