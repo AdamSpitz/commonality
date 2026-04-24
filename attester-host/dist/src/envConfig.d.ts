b36d22dc776cbed7eea36e4b238051fa223a6cc7
Adam Spitz <adam@acspitz.xyz>
2026-04-24 11:02:50 -0400

    Added stuff to .gitignore.

--- attester-host/dist/src/envConfig.d.ts
+++
@@ -1,3 +0,0 @@
-import type { AttesterHostConfig } from './config.js';
-export declare function loadAttesterHostConfigFromEnv(env?: NodeJS.ProcessEnv): AttesterHostConfig;
-//# sourceMappingURL=envConfig.d.ts.map
